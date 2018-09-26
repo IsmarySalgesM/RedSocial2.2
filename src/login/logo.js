0073ec31a7835b57bbf0236917e11cc789ac75ab
majosalazar <mariaj.salazarf@gmail.com>
2018-09-26 13:13:44 -0300

    cambio de ubicacion del logo

--- src/login/logo.js
+++ src/login/logo.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import '../login/login';
-import logoApp from '/home/laboratoria/again-red/src/logo-welcome2.png';
+import logoApp from '../img/logo-welcome2.png';
 class Logo extends Component {
 
   render() {
     return (
       <div className="center-align">
-        <img className="logoRedSocial" src={logoApp} />
+        <img className="logoRedSocial" src={logoApp} alt="logo welcome"/>
       </div>
     )
   }
